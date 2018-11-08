c5e5fce1023ed55d2076af11d4572b8cf49b0738
Maurílio <mauriliommachado@gmail.com>
2018-11-08 01:09:46 +0000

    init

---
+++ src/app/_services/authentication.service.ts
@@ -0,0 +1,27 @@
+﻿import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+import { environment } from '../../environments/environment';
+
+@Injectable()
+export class AuthenticationService {
+    constructor(private http: HttpClient) { }
+
+    login(username: string, password: string) {
+        return this.http.post<any>(`https://clinava.herokuapp.com/api/auth/signin`, { username: username, password: password })
+            .pipe(map(response => {
+                // login successful if there's a jwt token in the response
+                if (response && response.accessToken) {
+                    // store user details and jwt token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify(response));
+                }
+                return response;
+            }));
+    }
+
+    logout() {
+        // remove user from local storage to log user out
+        localStorage.removeItem('currentUser');
+    }
+}
