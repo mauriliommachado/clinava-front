f772235100b99edaa867179fdafa36a0a46fa193
maurilio.machado <maurilio.machado@feedzai.com>
2018-12-04 14:23:27 +0000

    asdasd

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8888'
+  apiUrl: 'https://clinava.herokuapp.com/api'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8888/api'
+  apiUrl: 'https://clinava.herokuapp.com/api'
 };
 
 /*
