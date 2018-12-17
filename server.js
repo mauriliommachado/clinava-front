59fcf0605e71b888d48d8fddcf0661ae04f3bcf4
Maurílio <mauriliommachado@gmail.com>
2018-12-17 23:08:55 +0000

    teste

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/clinava'));
+app.use(express.static('/dist/clinava'));
 
 app.get('/*', function(req,res) {
     
