44461d67f365dcbac03d7dcec13df92beccacc6b
Maurílio <mauriliommachado@gmail.com>
2018-12-17 21:56:24 +0000

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 ﻿import { Component, OnInit } from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Component({
     selector: 'app',
@@ -8,6 +9,10 @@
 export class AppComponent implements OnInit {
 
     ngOnInit() {
-        
+        if(environment.production){
+            if(location.protocol !== "https"){
+                location.href = 'https:' + window.location.href.substring(window.location.protocol.length);
+            }
+        }
     }
 }
