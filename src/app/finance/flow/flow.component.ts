12d1a78a4f591215d5b61cd442716ead3475ee39
maurilio.machado <maurilio.machado@feedzai.com>
2019-01-28 16:43:01 +0000

    report

--- src/app/finance/flow/flow.component.ts
+++ src/app/finance/flow/flow.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BillService } from '../../_services';
 import { Bill } from 'src/app/_models';
+import jsPDF from 'jspdf';
+import { autoTable } from 'jspdf-autotable';
 
 
 @Component({
@@ -26,6 +28,61 @@ export class FlowComponent implements OnInit {
   }
 
   onSubmit() {
+    this.validateFields();
+    this.billService.getBetween(this.initDate,
+      this.endDate,
+      this.paid,
+      this.toPay,
+      this.received,
+      this.toReceive).subscribe(resp => {
+        this.bills = resp
+      });
+  }
+
+  report() {
+    this.validateFields();
+    let jsPDF = require('jspdf');
+    require('jspdf-autotable');
+    var doc = new jsPDF({
+      orientation: 'landscape'});
+    doc.text('Fluxo de Caixa', 14, 22);
+
+    this.billService.getBetween(this.initDate,
+      this.endDate,
+      this.paid,
+      this.toPay,
+      this.received,
+      this.toReceive).subscribe(resp => {
+        var rows = [];
+        rows = resp;
+        rows.forEach(b => {
+          b.validUntil = new Date(b.validUntil).toLocaleDateString();
+          b.paymentMethod = b.paymentMethod.description;
+          b.status = b.status == 1? "ABERTO": "FECHADO";
+          b.nature = b.nature == 1? "RECEITA": "DESPESA";
+          b.category = b.category == 1? "FIXO": "VARIAVEL";
+        })
+        doc.autoTable({
+          head: [{
+            id: '#',
+            document: 'Nº',
+            description: 'Descrição',
+            category: 'Cat.',
+            validUntil: 'Validade',
+            value: 'Valor',
+            paymentMethod: 'Forma Pgto.',
+            nature: 'Natureza',
+            status: 'Status'
+          }],
+          body: rows,
+          startY: 30,
+        });
+        doc.save('table.pdf');
+      });
+    
+  }
+
+  validateFields(){
     if (!this.initDate) {
       this.initDate = new Date().toISOString().substring(0, 10);
     }
@@ -44,13 +101,6 @@ export class FlowComponent implements OnInit {
     if (!this.toReceive) {
       this.toReceive = false;
     }
-    this.billService.getBetween(this.initDate,
-      this.endDate,
-      this.paid,
-      this.toPay,
-      this.received,
-      this.toReceive).subscribe(resp => {
-        this.bills = resp
-      });
   }
+
 }
